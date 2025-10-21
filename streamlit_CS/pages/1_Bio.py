043479844bd4c3518a288e45a45f7a7560036a8e
JoshLapierre45 <jlapier1@msudenver.edu>
2025-10-21 16:28:23 -0600

    Commit1

--- streamlit_CS/pages/1_Bio.py
+++ streamlit_CS/pages/1_Bio.py
@@ -3,18 +3,18 @@ import streamlit as st
 st.title("👋 My Bio")
 
 # ---------- TODO: Replace with your own info ----------
-NAME = "Your Name"
-PROGRAM = "Your Program / Major / Role"
+NAME = "Josh Lapierre"
+PROGRAM = "Bachelors of Science/ Data Science Machine Learning/ Minors in Computer Science and Mathematics/ University of Connecticut"
 INTRO = (
-    "Write 2–3 sentences about yourself: what you’re studying/working on, "
-    "what excites you about data visualization or computing, etc."
+    "One project that I am particularly proud of is being part of a team that helps the SDA TAP Lab that also works with the United States Space Force. Our main goal is to create a benchmark dataset for satellite imagery that can be used to train machine learning models to identify and classify objects in space. This project is exciting because it combines my interests in data science, machine learning, and space exploration. "
+    "What excites me about data science and machine learning is how they merge logic, creativity, and innovation. Through my work in projects like the SDA TAP Lab and space domain awareness research, I’ve seen how ML can make sense of massive tracking data and enhance space operations."
 )
 FUN_FACTS = [
-    "I love …",
-    "I’m learning …",
-    "I want to build …",
+    "I love hunting and shooting sports. I find the precision and discipline required in these activities to be both challenging and rewarding. Firearms encorporate a multitude of different fields of study including physics, engineering, and even psychology. I enjoy learning about the mechanics of different firearms and the history behind them.",
+    "I’m learning Data Visualizations and how to create compelling stories with data. I believe that effective visualizations can make complex information more accessible and engaging, which is crucial in today’s data-driven world.",
+    "I want to build some kind of app that uses machine learning dictate the best ballistics for long-range shooting based on environmental factors like wind, humidity, and temperature. This would be a fun way to combine my interests in data science and shooting sports.",
 ]
-PHOTO_PATH = "your_photo.jpg"  # Put a file in repo root or set a URL
+PHOTO_PATH = "Headshot.jpg"  # Put a file in repo root or set a URL
 
 # ---------- Layout ----------
 col1, col2 = st.columns([1, 2], vertical_alignment="center")
